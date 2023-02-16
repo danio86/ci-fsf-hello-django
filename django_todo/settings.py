bf3274c20b716d5f4eeb939f5b3a9547b81b8c4c
danio86 <d.harder@gmx.at>
2023-02-16 13:51:59 +0000

    add add_item.html incl form+ link zu database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-)ovlz*=w3fw9h3p!36l9)uno#y&f07gn%1y5o4*&^6*kei=f5j'
+SECRET_KEY = 'django-insecure-)ovlz*=w3fw9h3p!36l9)uno#y&f07gn%1y5o4*&^6*kei=f5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
